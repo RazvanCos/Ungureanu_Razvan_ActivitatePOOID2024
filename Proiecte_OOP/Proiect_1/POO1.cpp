6f806cdd29e8eeaf481347d96de02e6c49be1b56
RazvanCos <urc26@yahoo.com>
2024-11-06 22:29:49 +0200

    Faza1: Modificari clase si creare obiecte

--- Proiecte_OOP/Proiect_1/POO1.cpp
+++ Proiecte_OOP/Proiect_1/POO1.cpp
@@ -3,118 +3,231 @@
 using namespace std;
 
 class Angajat {
+
 public:
 	string nume;
+	string pozitie;
+	float salariu;
+	int nrAbilitati;
 	const int id;
-	static int numarAngajati;
-	float* salariu;
+	static int nrAngajati;
+	string* abilitati;
 
-	// Constructori
-	Angajat() :id(++numarAngajati) {
+
+	// constructor implicit
+	Angajat():id(++nrAngajati) {
 		this->nume = "Necunoscut";
-		this->salariu = nullptr; // salariu default
+		this->pozitie = "Necunoscuta";
+		this->salariu = 1000;
+		this->nrAbilitati = 0;
+		this->abilitati = nullptr;
 	};
 
-	Angajat(string numeAngajat) :id(++numarAngajati) {
-		this->nume = numeAngajat;
-		this->salariu = new float(1500);
+	// constructor partial
+	Angajat(string nume, string pozitie, float salariu):id(++nrAngajati){
+		this->nume = nume;
+		this->pozitie = pozitie;
+		this->salariu = salariu;
 	}
 
-	Angajat(string numeAngajat, float salariuAngajat) : id(++numarAngajati){
+	// constructor complet
+	Angajat(string numeAngajat, string pozitie, float salariu, int nrAbilitati, string* abilitati) :id(++nrAngajati) {
 		this->nume = numeAngajat;
-		this->salariu = new float(salariuAngajat);
+		this->pozitie = pozitie;
+		this->salariu = salariu;
+		this->nrAbilitati = nrAbilitati;
+		// alocara memorie dinamica pentru vectorul de stringuri care contile abilitati
+		this->abilitati = new string[nrAbilitati];
+		// Deep copy -> copierea elem din vectorul primiit in vectorul nou creat al obiectului
+		for (int i = 0; i < nrAbilitati; i++) {
+			this->abilitati[i] = abilitati[i];
+		}		
 	}
 
-	static int getTotalAngajati() { // functie statica
-		return numarAngajati;
-	};
-
-	// destructor
-
+	// Destructor pentru eliberarea memoriei
 	~Angajat() {
-		delete salariu;
+		if (this->abilitati != nullptr) {
+			delete[] abilitati;
+		}
+	}
+	
+	// Metoda pentru afisarea angajatilor
+	void afisreAngajat() {
+		cout << "Angajatul #" << this->id << ":" << this->nume << ", pozitie: " << this->pozitie
+			<< ", salariu: " << this->salariu << endl;
+		if (this->abilitati && nrAbilitati > 0) {
+			cout << "Abilitati: "<<endl;
+			for (int i = 0; i < nrAbilitati; i++) {
+				cout << this->abilitati[i] << endl;
+			}
+			cout << endl;
+		}
+		else {
+			cout << "Nu are abilitati." << endl;
+		}
 	}
 };
 
-int Angajat::numarAngajati = 0;
+// declararea si intializarea membrului static in afara clasei
+int Angajat::nrAngajati = 0;
 
 
-class Manager {
+class Recrutare {
+
 public:
-	string nume;
-	static int numarManageri;
-	const int idManager;
-	int* echipa;
+	const int id;
+	string numeCandidat;
+	string pozitie;
+	string stadiu;
+	int nrFeedback;
+	static int nrRecrutari;
+	string* feedback;
 
-	// constructori
-
-	Manager(): idManager(++numarManageri) {
-		this->nume = "Necunoscut";
-		this->echipa = new int(5);
+	// constructor implicit
+	Recrutare(): id(++nrRecrutari){
+		this->numeCandidat = "Necunoscut";
+		this->pozitie = "Necunoscuta";
+		this->stadiu = "In asteptare";
+		this->nrFeedback = 0;
+		this->feedback = nullptr;
 	}
 
-	Manager(string numeManager): idManager(++numarManageri){
-		this->nume = numeManager;
-		this->echipa = new int(3);
+	// constructor complet
+	Recrutare(string numeCandidat, string pozitie, string stadiu, int nrFeedback, string* feedback): id(++nrRecrutari){
+		this->numeCandidat = numeCandidat;
+		this->pozitie = pozitie;
+		this->stadiu = stadiu;
+		this->nrFeedback = nrFeedback;
+		// alocare memorie
+		this->feedback = new string[nrFeedback];
+		for (int i = 0; i < nrFeedback; i++) {
+			this->feedback[i] = feedback[i];
+		}
 	}
 
-	Manager(string numeManager, int numarEchipa) : idManager(++numarManageri) {
-		this->nume = numeManager;
-		this->echipa = new int(numarEchipa);
+	// constructor partial
+	Recrutare(string numeCandidat, string pozitie): id(++nrRecrutari){
+		this->numeCandidat = numeCandidat;
+		this->pozitie = pozitie;
+		this->stadiu = "On hold";
+		this->nrFeedback = 0;
+		this->feedback = nullptr;
 	}
 
-	static int getTotalManageri() {
-		return numarManageri;
+	~Recrutare(){
+		if (this->feedback != nullptr) {
+			delete[] feedback;
+		}
 	}
 
-	// destructor
-	~Manager() {
-		delete echipa;
+	void afisareRecrutare() {
+		cout << "Recrutare #" << this->id << ": Candidat: " << this->numeCandidat << ", pozitie: " << this->pozitie << ", este in stadiul: " << this->stadiu << endl;
+		if (this->feedback && nrFeedback > 0) {
+			cout << "Feedback: " << endl;
+			for (int i = 0; i < nrFeedback; i++) {
+				cout << this->feedback[i] << endl;
+			}
+		}
+		else {
+			cout << "Candidatul: " << this->numeCandidat << " nu are feedback momentan!" << endl;
+		}
 	}
-
 };
 
-int Manager::numarManageri = 0;
+int Recrutare::nrRecrutari = 0;
 
-class Proiect {
+class Organizatie {
 public:
-	string titlu;
-	static int numarProiecte;
-	const int idProiect;
-	int* durata;
+	const int id;
+	static int nrUnitati;
+	string denumire;
+	string manager;
+	int nrObiective;
+	string* obiective;
 
-	Proiect() : idProiect(++numarProiecte) {
-		this->titlu = "Necunoscut";
-		this->durata = new int(6);
+	// constructor implicit
+	Organizatie(): id(++nrUnitati){
+		this->denumire = "Necunoscuta";
+		this->manager = "necunoscut";
+		this->nrObiective = 0;
+		this->obiective = nullptr;
 	};
 
-	Proiect(string titluProiect) :idProiect(++numarProiecte) {
-		this->titlu = titluProiect;
-		this->durata = new int(12);
+	// constructor complet
+	Organizatie(string denumire, string manager, int nrObiective, string* obiective): id(++nrUnitati){
+		this->denumire = denumire;
+		this->manager = manager;
+		this->nrObiective = nrObiective;
+		this->obiective = new string[nrObiective];
+		for (int i = 0; i < nrObiective; i++) {
+			this->obiective[i] = obiective[i];
+		}
 	}
 
-	Proiect(string titluProiect, int durataProiect) : idProiect(++numarProiecte) {
-		this->titlu = titluProiect;
-		this->durata = new int(durataProiect);
+	// constructor partial
+	Organizatie(string denumire, string manager): id(++nrUnitati){
+		this->denumire = denumire;
+		this->manager = manager;
+		this->nrObiective = 0;
+		this->obiective = nullptr;
 	}
 
-	static int getTotalProiecte() {
-		return numarProiecte;
+
+	~Organizatie(){
+		if (this->obiective != nullptr) {
+			delete[] obiective;
+		}
 	}
 
-	~Proiect() {
-		delete durata;
+	void afisareOrganizatie() {
+		cout << "Organizatie: #" << this->id << ", Denumire: " << this->denumire << ", Manager: " << this->manager << endl;
+		if (this->obiective && this->nrObiective > 0) {
+			cout << "Obiective: " << endl;
+			for (int i = 0; i < this->nrObiective; i++) {
+				cout << this->obiective[i] << endl;
+			}
+		}
+		else {
+			cout << "Organizatia: " << this->denumire << " nu are obiective!";
+		}
 	}
 };
 
-int Proiect::numarProiecte = 0;
+int Organizatie::nrUnitati = 0;
+
+
 
 
 int main() {
-
+	string abilitati[] = { "C++", "Java", "Databases", "Frontend", "Backend" };
 	Angajat a1;
-	Angajat a2("Gigel");
-	Angajat a3("Popescu Maria", 16);
-	cout << "Numarul total de angajati este: " << a1.getTotalAngajati() << endl;
+	Angajat a2("Radu", "software developer", 2000, 5, abilitati);
+	a1.afisreAngajat();
+	cout << endl;
+	a2.afisreAngajat();
+	cout << endl;
+
+
+	string feedback[] = { "Inteviu tehnic pozitiv", "Experienta relevanta" };
+	Recrutare r1("Virgil", "software developer", "Interviu finalizat", 2, feedback);
+	Recrutare r2;
+	Recrutare r3;
+	r1.afisareRecrutare();
+	cout << endl;
+	r2.afisareRecrutare();
+	cout << endl;
+	r3.afisareRecrutare();
+
+	string obiective[] = {"Imbunatatire performanta", "Extindere pe piata", "Reducere costuri"};
+	/*Organizatie org1("TechCorp", "Popescu Ionut", 3, obiective);*/
+	Organizatie org2("ITSolution", "Ionescu Marian");
+
+	//org1.afisareOrganizatie();
+	cout << endl;
+	org2.afisareOrganizatie();
+	cout << endl;
+
+
+	return 0;
 
 }
